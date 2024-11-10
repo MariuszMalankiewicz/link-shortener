33f367ee58a7646eefc64187d68bc3c788dff881
MariuszMalankiewicz <m2ario123@wp.pl>
2024-11-10 17:38:35 +0100

    add vue, vue plugin, vue router and configuration to laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import App from './App.vue';
+import axios from 'axios';
+import { createRouter, createWebHistory } from 'vue-router';
+import routes from './routes';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+window.axios = axios;
+axios.defaults.baseURL = '/api';
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
